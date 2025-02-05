f9164c43ee147e1bb13802d997dcf2131c4a9b4b
devhimelali <dev.himelali@gmail.com>
2025-02-05 21:20:46 +0600

    Brand update and delete not completed

---
+++ resources/views/admin/brand/edit.blade.php
@@ -0,0 +1,121 @@
+<form action="{{ route('brands.update', $brand->slug) }}" class="dynamic-form" method="POST" id="editForm" enctype="multipart/form-data">
+    @csrf
+    @method('PUT')
+    <div class="row g-3">
+        <div class="col-xl-7">
+            <div class="my-3">
+                <div>
+                    <label for="name" class="form-label">Name<span class="text-danger">*</span></label>
+                    <input type="text" class="form-control" name="name" id="name"
+                        placeholder="Enter Category Name" value="{{ $brand->name }}">
+                </div>
+            </div>
+            <div class="my-3">
+                <div>
+                    <label for="slug" class="form-label">Slug <span class="text-danger">*</span></label>
+                    <input type="text" id="slug" name="slug" class="form-control"
+                        placeholder="Enter category slug" value="{{ old('slug', $brand->slug) }}">
+                </div>
+            </div>
+            <div class="my-3">
+                <div>
+                    <label for="status" class="form-label">Status<span class="text-danger">*</span></label>
+                    <select class="form-select" name="status" id="brandStatus">
+                        <option selected value="">Select Status</option>
+                        @foreach ($statuses as $status)
+                            <option value="{{ $status->value }}" {{ $brand->status->value == $status->value ? 'selected' : '' }}>{{ $status->description() }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="my-3">
+                <div>
+                    <label for="description" class="form-label">Description<span class="text-danger">*</span></label>
+                    <textarea name="description" id="description" class="form-control" rows="4">{{$brand->description}}</textarea>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xxl-5 pt-4">
+            <div class="d-flex justify-content-center align-items-center">
+                <div class="text-center">
+                    <div class="image-container" id="imagePreview">
+                        @php
+                            $image = $brand->image ? asset($brand->image) : asset('assets/placeholder-image.webp');
+                        @endphp
+                        <img src="{{ $image }}" class="previewImg" alt="Image Preview">
+                        <button type="button" class="remove-btn" id="removeImage"
+                            style="display:none;">&times;</button>
+                    </div>
+                    <input type="file" name="image" id="imageUpload" class="d-none" accept="image/*">
+                    <label for="imageUpload" class="upload-label d-block mt-3">Choose a Category
+                        Image</label>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-12">
+            <div class="hstack gap-2 justify-content-end">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary btn-load submit-btn">
+                    <span class="submit-text">Submit</span>
+                    <span class="d-flex align-items-center d-none loader">
+                        <span class="flex-grow-1 me-2">Submitting...</span>
+                        <span class="spinner-grow flex-shrink-0" role="status"><span
+                                class="visually-hidden">Loading...</span></span>
+                    </span>
+                </button>
+            </div>
+        </div>
+    </div>
+</form>
+
+<script>
+    $(document).ready(function() {
+        $('#editForm').on('submit', function(e) {
+            e.preventDefault();
+
+            // Show loader, hide submit button text
+            $('.submit-text').addClass('d-none');
+            $('.loader').removeClass('d-none');
+
+            let formData = new FormData(this);
+
+            $.ajax({
+                url: $(this).attr('action'), // Form action (route)
+                type: 'POST',
+                data: formData,
+                processData: false,
+                contentType: false,
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+                },
+                success: function(response) {
+                    if (response.status === "success") {
+                        notify(response.status, response.message);
+                        $('#addForm')[0].reset(); // Reset form
+                        $('#editModal').modal('hide'); // Close modal
+                        $('#dataTable').DataTable().ajax
+                            .reload(); // Reload DataTable if needed
+                    }
+                },
+                error: function(xhr) {
+                    // Show submit button text and hide loader
+                    $('.submit-text').removeClass('d-none');
+                    $('.loader').addClass('d-none');
+
+                    // Handle validation errors
+                    let errors = xhr.responseJSON.errors;
+                    $(".text-danger").remove(); // Remove old error messages
+                    if (errors) {
+                        $.each(errors, function(key, value) {
+                            let inputField = $('[name="' + key + '"]');
+                            inputField.after('<small class="text-danger">' + value[
+                                0] + '</small>');
+                        });
+                    }
+                }
+            });
+        });
+    });
+</script>
---
+++ resources/views/admin/brand/index.blade.php
@@ -0,0 +1,339 @@
+@extends('layouts.app')
+@section('title', 'Brands')
+@section('content')
+    {{--  Start breadcrumb  --}}
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                <h4 class="mb-sm-0">Brands</h4>
+
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                        <li class="breadcrumb-item active">Brands</li>
+                    </ol>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    {{--  End breadcrumb  --}}
+
+    <div class="row mb-5">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header d-flex align-items-center">
+                    <div class="ms-auto">
+                        <a href="#" class="btn btn-primary add-btn" data-bs-toggle="modal" data-bs-target="#addModal">
+                            <i class="bi bi-plus-circle align-baseline me-1"></i> Add Brand
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <div class="table-responsive mt-3 mb-1">
+                        <table id="dataTable" class="table align-middle ">
+                            <thead class="table-light">
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Image</th>
+                                    <th>Name</th>
+                                    <th>Description</th>
+                                    <th>Status</th>
+                                    <th style="width: 140px">Actions</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div><!--end card-->
+        </div><!--end col-->
+    </div>
+
+    <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="exampleModalgridLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content p-3">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalgridLabel">Create Brand</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('brands.store') }}" class="dynamic-form" method="POST" id="addForm"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <div class="row g-3">
+                            <div class="col-xl-7">
+                                <div class="my-3">
+                                    <div>
+                                        <label for="name" class="form-label">Name<span
+                                                class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" name="name" id="name"
+                                            placeholder="Enter Category Name">
+                                    </div>
+                                </div>
+                                <div class="my-3 d-none slugWrapper">
+                                    <div>
+                                        <label for="slug" class="form-label">Slug <span
+                                                class="text-danger">*</span></label>
+                                        <input type="text" id="slug" name="slug" class="form-control"
+                                            placeholder="Enter category slug" value="{{ old('slug') }}">
+                                    </div>
+                                </div>
+                                <div class="my-3">
+                                    <div>
+                                        <label for="status" class="form-label">Status<span
+                                                class="text-danger">*</span></label>
+                                        <select class="form-select" name="status" id="brandStatus">
+                                            <option selected value="">Select Status</option>
+                                            @foreach ($statuses as $status)
+                                                <option value="{{ $status->value }}">{{ $status->description() }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="my-3">
+                                    <div>
+                                        <label for="description" class="form-label">Description<span
+                                                class="text-danger">*</span></label>
+                                        <textarea name="description" id="description" class="form-control" rows="4"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xxl-5 pt-4">
+                                <div class="d-flex justify-content-center align-items-center">
+                                    <div class="text-center">
+                                        <div class="image-container" id="imagePreview">
+                                            <img src="{{ asset('assets/placeholder-image.webp') }}" class="previewImg"
+                                                alt="Image Preview">
+                                            <button type="button" class="remove-btn" id="removeImage"
+                                                style="display:none;">&times;</button>
+                                        </div>
+                                        <input type="file" name="image" id="imageUpload" class="d-none"
+                                            accept="image/*">
+                                        <label for="imageUpload" class="upload-label d-block mt-3">Choose a Category
+                                            Image</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="hstack gap-2 justify-content-end">
+                                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary btn-load submit-btn">
+                                        <span class="submit-text">Submit</span>
+                                        <span class="d-flex align-items-center d-none loader">
+                                            <span class="flex-grow-1 me-2">
+                                                Submitting...
+                                            </span>
+                                            <span class="spinner-grow flex-shrink-0" role="status">
+                                                <span class="visually-hidden">Loading...</span>
+                                            </span>
+                                        </span>
+                                    </button>
+                                </div>
+                            </div>
+                            <!--end col-->
+                        </div>
+                        <!--end row-->
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">Edit Brand</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" id="editModalContent">
+                    <!-- The AJAX response (edit form) will be loaded here -->
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('page-script')
+    <script src="{{ asset('assets/cdn/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/cdn/datatables/dataTables.bootstrap5.min.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            $('#dataTable').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('brands.index') }}",
+                columns: [{
+                        data: 'id',
+                        name: 'id'
+                    },
+                    {
+                        data: 'image',
+                        name: 'image'
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+                    {
+                        data: 'description',
+                        name: 'description'
+                    },
+                    {
+                        data: 'status',
+                        name: 'status',
+                    },
+                    {
+                        data: 'actions',
+                        name: 'actions',
+                        orderable: false,
+                        searchable: false
+                    },
+                ]
+            });
+
+            $("#addForm").on("submit", function(e) {
+                e.preventDefault();
+                let formData = new FormData(this);
+                let submitButton = $(".submit-btn");
+                let submitText = $(".submit-text");
+                let loader = $(".loader");
+
+                submitText.addClass("d-none");
+                loader.removeClass("d-none");
+                submitButton.prop("disabled", true);
+
+                $.ajax({
+                    url: "{{ route('brands.store') }}",
+                    type: "POST",
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    headers: {
+                        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+                    },
+                    success: function(response) {
+                        if (response.status === "success") {
+                            notify(response.status, response.message);
+                            $("#addForm")[0].reset();
+                            $(".previewImg").attr("src",
+                                "{{ asset('assets/placeholder-image.webp') }}");
+
+                            if ($.fn.DataTable.isDataTable("#dataTable")) {
+                                $("#dataTable").DataTable().ajax.reload();
+                            }
+
+                            submitText.removeClass("d-none");
+                            loader.addClass("d-none");
+                            submitButton.prop("disabled", false);
+                            $('#addModal').modal('hide');
+                        }
+                    },
+                    error: function(xhr) {
+                        let errors = xhr.responseJSON.errors;
+                        $(".text-danger").remove();
+                        if (errors) {
+                            $.each(errors, function(key, value) {
+                                let inputField = $('[name="' + key + '"]');
+                                inputField.after('<small class="text-danger">' + value[
+                                    0] + '</small>');
+                                notify('error', value[0]);
+                            });
+                        }
+                    },
+                    complete: function() {
+                        submitText.removeClass("d-none");
+                        loader.addClass("d-none");
+                        submitButton.prop("disabled", false);
+                    }
+                });
+            });
+        });
+
+        function editCategory(slug) {
+            $.ajax({
+                url: "{{ route('brands.edit', 'slug') }}".replace('slug',
+                    slug),
+                type: "GET",
+                success: function(response) {
+                    $("#editModalContent").html(response); // Load the response (view) into modal
+                    $("#editModal").modal("show"); // Show the modal
+                },
+                error: function(xhr) {
+                    notify('error', "Failed to load the brand edit form.");
+                }
+            });
+        }
+    </script>
+
+    <script>
+        $(document).ready(function() {
+            $('#imageUpload').change(function(event) {
+                const file = event.target.files[0];
+                if (file) {
+                    const reader = new FileReader();
+                    reader.onload = function(e) {
+                        $('.previewImg').attr('src', e.target.result);
+                        $('#removeImage').show(); // Show remove button
+                    };
+                    reader.readAsDataURL(file);
+                }
+            });
+
+            $('#removeImage').click(function() {
+                $('.previewImg').attr('src',
+                    '{{ asset('assets/placeholder-image.webp') }}'); // Reset image to placeholder
+                $('#imageUpload').val(''); // Clear file input
+                $('#removeImage').hide(); // Hide remove button
+            });
+        });
+    </script>
+@endsection
+@section('page-css')
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/cdn/datatables/dataTables.bootstrap5.min.css') }}">
+    <style>
+        .image-container {
+            position: relative;
+            width: 200px;
+            height: 200px;
+            border: 2px dashed #ccc;
+            border-radius: 10px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            overflow: hidden;
+            background-color: #f8f9fa;
+        }
+
+        .image-container img {
+            max-width: 100%;
+            max-height: 100%;
+            object-fit: cover;
+        }
+
+        .upload-label {
+            cursor: pointer;
+            color: #007bff;
+            font-weight: bold;
+        }
+
+        .remove-btn {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            background-color: rgba(0, 0, 0, 0.6);
+            color: white;
+            border: none;
+            border-radius: 50%;
+            width: 25px;
+            height: 25px;
+            font-size: 14px;
+            display: none;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
+@endsection
