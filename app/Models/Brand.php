b52f0cd41a9211292708eeb6e326793835fd1a64
devhimelali <dev.himelali@gmail.com>
2025-02-05 19:40:01 +0600

    Brand model and migration added

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use App\Enum\StatusEnum;
 use Illuminate\Database\Eloquent\Model;
 
 class Brand extends Model
 {
-    //
+    protected $fillable = [
+        "name",
+        "slug",
+        "image",
+        "description",
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'status' => StatusEnum::class,
+        ];
+    }
 }
