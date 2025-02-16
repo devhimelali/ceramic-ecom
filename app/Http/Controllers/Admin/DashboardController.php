437d3ca239b97c8f2dc522f1ad0708ac4f433231
sakib-1996 <abdullahmohammadsakib8@gmail.com>
2025-02-16 18:12:25 +0600

    dashboard update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,16 +2,30 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use App\Models\ProductQuery;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $productQuery = ProductQuery::selectRaw('
+        MONTH(created_at) as month,
+        MONTHNAME(created_at) as month_name,
+        COUNT(*) as total
+    ')
+            ->whereYear('created_at', Carbon::now()->year)
+            ->groupBy('month', 'month_name') // Include both month and month_name in GROUP BY
+            ->orderBy('month')
+            ->pluck('total', 'month_name'); // Fetch month name as key and total count as value
+
         $data = [
-            'active' => 'dashboard'
+            'active' => 'dashboard',
+            'productQuery' => $productQuery // Pass query result to the view
         ];
+
         return view('admin.dashboard', $data);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,63 @@
 @extends('layouts.app')
 @section('title', 'Dashboard')
 @section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                <h4 class="mb-sm-0">CRM</h4>
+
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboards</a></li>
+                        <li class="breadcrumb-item active">CRM</li>
+                    </ol>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xxl col-sm-6">
+            <div class="card overflow-hidden">
+                <div class="card-body">
+                    <h4><span class="counter-value" data-target="368.24">368.24</span></h4>
+                    <p class="text-muted mb-4">Total Product</p>
+                </div>
+                <div class="progress progress-sm rounded-0" role="progressbar" aria-valuenow="76" aria-valuemin="0"
+                    aria-valuemax="100">
+                    <div class="progress-bar" style="width: 100%"></div>
+                </div>
+            </div>
+        </div><!--end col-->
+        <div class="col-xxl col-sm-6">
+            <div class="card overflow-hidden">
+                <div class="card-body">
+                    <h4><span class="counter-value">1,454.71</span> </h4>
+                    <p class="text-muted mb-4">Total Brand</p>
+                </div>
+                <div class="progress progress-sm rounded-0" role="progressbar" aria-valuenow="88" aria-valuemin="0"
+                    aria-valuemax="100">
+                    <div class="progress-bar bg-secondary" style="width: 100%"></div>
+                </div>
+            </div>
+        </div><!--end col-->
+        <div class="col-xxl col-sm-6">
+            <div class="card overflow-hidden">
+                <div class="card-body">
+                    <div class="avatar-sm float-end">
+                        <div class="avatar-title bg-danger-subtle text-danger fs-3xl rounded">
+                            <i class="bi bi-broadcast"></i>
+                        </div>
+                    </div>
+                    <h4><span class="counter-value" data-target="33.37">33.37</span>%</h4>
+                    <p class="text-muted mb-4">Unread Contact</p>
+                </div>
+                <div class="progress progress-sm rounded-0" role="progressbar" aria-valuenow="18" aria-valuemin="0"
+                    aria-valuemax="100">
+                    <div class="progress-bar bg-danger" style="width: 100%"></div>
+                </div>
+            </div>
+        </div><!--end col-->
+    </div>
 @endsection
