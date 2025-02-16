d4c735306abbe634277573d320135a58b80dc0d8
devhimelali <dev.himelali@gmail.com>
2025-02-16 18:13:40 +0600

    Admin Dashboard stat added

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,15 +2,29 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\ContactUs;
+use App\Models\ProductQuery;
 use Illuminate\Http\Request;
+use App\Enum\ProductQueryStatus;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $data = [
-            'active' => 'dashboard'
+            'active' => 'dashboard',
+            'totalProducts' => Product::count(),
+            'totalCategories' => Category::count(),
+            'totalBrands' => Brand::count(),
+            'totalQueries' => ProductQuery::count(),
+            'totalPendingQueries' => ProductQuery::where('status', ProductQueryStatus::PENDING)->count(),
+            'pendingQueries' => ProductQuery::where('status', ProductQueryStatus::PENDING)->get(),
+            'totalUnreadContact' => ContactUs::where('is_read', 0)->count(),
+            'totalPendingReplay' => ContactUs::where('is_replied', 0)->count(),
         ];
         return view('admin.dashboard', $data);
     }
