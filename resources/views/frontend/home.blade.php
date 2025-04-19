99778709be6c1846870f7331bcc0b7dcc28ee4a2
devhimelali <dev.himelali@gmail.com>
2025-04-19 11:35:43 +0600

    feat: show 'Product not found' message when no product is found

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -609,7 +609,7 @@
 
 
             <div class="row">
-                @foreach ($products as $product)
+                @forelse ($products as $product)
                     <div class="col-xl-3 col-lg-4 col-md-6 col-6 product_item">
                         <div class="product__item wow fadeInUp" data-wow-duration='1500ms' data-wow-delay='000ms'>
                             @php
@@ -684,7 +684,9 @@
                             </div><!-- /.product-content -->
                         </div><!-- /.product-item -->
                     </div><!-- /.col-md-6 col-lg-4 -->
-                @endforeach
+                @empty
+                    <h5 class="text-center">No products found.</h5>
+                @endforelse
             </div><!-- /.row -->
         </div><!-- /.container -->
     </section><!-- /.product-home -->
@@ -701,7 +703,7 @@
                 <!-- /.sec-title__title -->
             </div><!-- /.sec-title -->
             <div class="row">
-                @foreach ($topSellingProducts as $product)
+                @forelse ($topSellingProducts as $product)
                     <div class="col-xl-3 col-lg-4 col-md-6 col-6 product_item">
                         <div class="product__item wow fadeInUp" data-wow-duration='1500ms' data-wow-delay='000ms'>
                             @php
@@ -776,7 +778,9 @@
                             </div><!-- /.product-content -->
                         </div><!-- /.product-item -->
                     </div><!-- /.col-md-6 col-lg-4 -->
-                @endforeach
+                @empty
+                    <h5 class="text-center">No products found.</h5>
+                @endforelse
             </div><!-- /.row -->
         </div><!-- /.container -->
     </section><!-- /.product-home -->
